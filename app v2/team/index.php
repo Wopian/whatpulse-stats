b23b4047b7e15ebcf3ff9d86dc6ea32db8775a29
James Harris <wopiancitizen123@gmail.com>
2014-07-12 21:57:43 +0100

    Add User/Team pages

---
+++ app v2/team/index.php
@@ -0,0 +1,361 @@
+<!doctype html>
+<html prefix="og: http://ogp.me/ns#">
+<head>
+  <title>Reddit - WhatPulse Team Stats</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon" />  
+  <link rel="stylesheet" href="../assets/css/main.css" type="text/css" />
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="../assets/javascript/chart.js"></script>
+  <meta property="og:title" content="WhatPulse Stats" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://dev.boomcraft.co.uk/4" />
+  <meta property="og:image" content="http://dev.boomcraft.co.uk/4/assets/logo.png" />
+  <script>
+    $(function() {
+      $("span.pie").peity("pie");
+
+      $(".pie-a").peity("pie", {
+        fill: ["#E1E6EF", "#177BBB"]
+      });
+
+      $(".pie-b").peity("pie", {
+        fill: ["#177BBB", "#E1E6EF"]
+      });
+    });
+
+  </script>
+</head>
+
+<body>
+
+<header>
+  <div><h1><a href="../">WhatPulse Stats</a></h1></div>
+  <nav><a href="../user">User</a> <a href="./">Team</a> <a href="../compare">Compare</a></nav>
+</header>
+
+<main>
+
+<div class="bg">
+  <p>
+    Team Info
+  </p>
+
+  <div class="Grid">
+
+    <div class="Grid-cell">
+      <div>
+        <small>Team</small>
+        <span class="h2">Reddit</span>
+        <small>ID</small>
+      </div>
+    </div>
+
+        <div class="Grid-cell">
+      <div>
+        <small>Founder</small>
+        <span class="h2">Dung Fungus</span>
+        <small>&nbsp;</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Founded</small>
+        <span class="h2">3,914.84</span>
+        <small>Days Ago</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Members</small>
+        <span class="h2">230</span>
+        <small>&nbsp;</small>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="bg2">
+  <p>
+    Team Stats
+  </p>
+
+  <div class="Grid">
+
+    <div class="Grid-cell">
+      <div>
+        <small>Keys</small>
+        <span class="h2">1.9 B</span>
+        <small>11th</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Clicks</small>
+        <span class="h2">977.3 M</span>
+        <small>9th</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Total Keys & Clicks</small>
+        <span class="h2">2.9 B</span>
+        <small>&nbsp;</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Keys & Clicks Ratio</small>
+        <span class="h2 chart">1.98:1<span class="pie pie-a" data-diameter="24">1,1.98</span></span>
+        <small>Keys To Clicks</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Downloaded</small>
+        <span class="h2">168.8 TB</span>
+        <small>6th</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Uploaded</small>
+        <span class="h2">95.5 TB</span>
+        <small>11th</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Total Download & Upload</small>
+        <span class="h2">264.3 TB</span>
+        <small>&nbsp;</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Download & Upload Ratio</small>
+        <span class="h2 chart">1.77:1<span class="pie pie-a" data-diameter="24">1,1.77</span></span>
+        <small>Download To Upload</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Uptime</small>
+        <span class="h2">772,981.5</span>
+        <small>4th</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <small>Average Uptime</small>
+        <span class="h2">3,360.79</span>
+        <small>Hours Per User</small>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="bg">
+  <p>
+    Key Stats
+  </p>
+
+  <div class="Grid">
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">343.7</span>
+        <small>Per Minute</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">20,626.1</span>
+        <small>Per Hour</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">495,025.1</span>
+        <small>Per Day</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">15.1 M</span>
+        <small>Per Month</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">180.8 M</span>
+        <small>Per Year</small>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="bg2">
+  <p>
+    Click Stats
+  </p>
+
+  <div class="Grid">
+
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">173.38</span>
+        <small>Per Minute</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">10,402.5</span>
+        <small>Per Hour</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">249,661.1</span>
+        <small>Per Day</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">7.6 M</span>
+        <small>Per Month</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">91.19 M</span>
+        <small>Per Year</small>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="bg">
+  <p>
+    Download Stats
+  </p>
+
+  <div class="Grid">
+
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">216.6 MB</span>
+        <small>Per Minute</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">12.7 GB</span>
+        <small>Per Hour</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">304.6 GB</span>
+        <small>Per Day</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">9.1 TB</span>
+        <small>Per Month</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">108.6 TB</span>
+        <small>Per Year</small>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="bg2">
+  <p>
+    Upload Stats
+  </p>
+
+  <div class="Grid">
+
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">122.53 MB</span>
+        <small>Per Minute</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">7.18 GB</span>
+        <small>Per Hour</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">172.31 GB</span>
+        <small>Per Day</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">5.12 TB</span>
+        <small>Per Month</small>
+      </div>
+    </div>
+
+    <div class="Grid-cell">
+      <div>
+        <span class="h2">61.46 TB</span>
+        <small>Per Year</small>
+      </div>
+    </div>
+
+  </div>
+
+</main>
+
+<footer>
+<pre>dev.boomcraft.co.uk/4</pre>
+</footer>
+
+</body>
+</html>
